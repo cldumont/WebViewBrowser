ea0dd891c35391de40f4662881ba115ec0c610cb
Craig Dumont <cldumont77@comcast.net>
2018-03-06 16:53:04 -0500

    Initial Commit

---
+++ WebViewBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewBrowser
+//
+//  Created by Craig Dumont on 3/6/18.
+//  Copyright © 2018 Craig Dumont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
