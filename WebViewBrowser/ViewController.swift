7e0c56fb5ef56aa60dda70604ea0f7fe976f2802
Craig Dumont <cldumont77@comcast.net>
2018-03-06 17:10:49 -0500

    Initial setup of web view.

--- WebViewBrowser/ViewController.swift
+++ WebViewBrowser/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var webView: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let url:URL = URL(string: "https://www.apple.com")!
+        let urlRequest: URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
     }
 
     override func didReceiveMemoryWarning() {
