29fd35758110c1a5cca9e4dc89ec44ba0627e480
Craig Dumont <cldumont77@comcast.net>
2018-03-07 19:40:12 -0500

    Back and forward buttons are working. Textfield is also working properly.

--- WebViewBrowser/ViewController.swift
+++ WebViewBrowser/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
 
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -19,7 +19,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,11 +35,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         urlTextField.text = urlString
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         let urlString:String = urlTextField.text!
         let url:URL = URL(string: urlString)!
@@ -49,5 +46,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
 
+    @IBAction func backButton(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardButton(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
+        urlTextField.text = webView.url?.absoluteString
+    }
 }
 
